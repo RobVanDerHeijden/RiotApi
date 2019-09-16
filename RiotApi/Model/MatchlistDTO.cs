884337930772986a17b08678e9fe8a6b8930540c
Rob van der Heijden <413117@student.fontys.nl>
2019-09-16 17:16:54 +0200

    Alle Nodige Match DTO's + mappenstruktuur weggehaald
    Mappenstruktuur zou niet gaan werken, aangezien de MatchPositionDTO meerdere keren wordt gebruikt

---
+++ RiotApi/Model/MatchlistDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiotApi.Model
+{
+    public class MatchlistDTO
+    {
+        public Dictionary<string, MatchReferenceDTO> Matches { get; set; }
+        public int TotalGames { get; set; }
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
