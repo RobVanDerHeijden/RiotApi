c00325b6a844abe531435cb1a30b0ebb0f348b95
Rob van der Heijden <413117@student.fontys.nl>
2019-10-25 14:26:53 +0200

    Ranked icons size fixed + ranks now in tabs

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    function openCity(rankType) {
+        var i;
+        var x = document.getElementsByClassName("rank-tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(rankType).style.display = "block";
+    }
