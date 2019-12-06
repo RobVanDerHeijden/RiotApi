daf07be1e9267e4799aabd3dc2a542214effcc4c
Rob van der Heijden <413117@student.fontys.nl>
2019-12-06 15:57:49 +0100

    Cleanup/moved some files/folders

--- Model/Summoner/Rank.cs
+++ Model/Summoner/Rank.cs
@@ -7,13 +7,13 @@ namespace Model
 {
     public class Rank
     {
+        public string Tier { get; set; }
         [JsonProperty("rank")]
         public string Division { get; set; }
-        public string Tier { get; set; }
         public int LeaguePoints { get; set; } // LP
         public int Wins { get; set; }
         public int Losses { get; set; }
-        public Summoner Summoner { get; set; }
+        //public Summoner Summoner { get; set; }
         public League League { get; set; }
         public string QueueType { get; set; }
         public QueueType QueueTypeObject { get; set; }
